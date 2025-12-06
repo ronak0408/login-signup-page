869da27daa400942682e049624550a9463680629
Ronak <ronaksain0408@gmail.com>
2025-12-07 00:31:37 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('MongoDB connection error:', err));
 
